42c49b462b73e96b5e883c0a1713fefe253e7695
Nestor Cortina <nexckycort@gmail.com>
2025-12-17 13:23:45 -0500

    feat(payments-react): update package.json and remove Button component

--- packages/payments-react/src/index.tsx
+++ packages/payments-react/src/index.tsx
@@ -1,1 +1,8 @@
-export { Button } from './Button';
+export type {
+  AppearanceConfig,
+  CheckoutConfig,
+  PaymentSubmitPayload,
+} from '@bloque/payments-elements';
+export { PAYMENT_METHODS } from '@bloque/payments-elements';
+export type { BloqueCheckoutProps } from './bloque-checkout';
+export { BloqueCheckout } from './bloque-checkout';
